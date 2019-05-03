fb4f02bf083d5b6f1f025ad2a2499df43e49c96a
MartaVaqgut <martavaquerogutierrez@gmail.com>
2019-05-03 19:45:34 +0200

    Traer commit

--- config/passport.config.js
+++ config/passport.config.js
@@ -37,7 +37,7 @@ passport.use(new GoogleStrategy({
       done(null, newUser);
     })
   })
-  .catch(error => {
+  .catch((error) => {
     next(error)
   })
 
@@ -48,7 +48,9 @@ passport.use('Local-Auth', new LocalStrategy({
   passwordField: 'password'
 }, (email, password, next) => {
   User.findOne({ email: email})
+    
     .then(user => {
+      console.log('entra aqui')
       if (!user) {
         next(null, null, 'Invalid email or password')
       } else {
