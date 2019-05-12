43f3504ab05033f161166b6b354983d2021ed459
Juan Portillo <juanportillog@gmail.com>
2019-05-12 23:05:25 +0200

    bugs terminados, falta estilizar

--- routes/pedidos.routes.js
+++ routes/pedidos.routes.js
@@ -6,7 +6,7 @@ const secure = require('../middlewares/secure.mid');
 router.get('/cesta', secure.isAuthenticated, secure.checkRole('USER'), pedidosController.verPedidos);
 router.get('/historico', secure.isAuthenticated, secure.checkRole('USER'), pedidosController.historico)
 
-router.post('/:id/cesta/delete', secure.isAuthenticated, secure.checkRole('USER'), pedidosController.delete);
-router.post('/:id/cesta', secure.isAuthenticated, secure.checkRole('USER'), pedidosController.addPedido);
+router.post('/:id/cesta/delete', secure.isAuthenticated, pedidosController.delete);
+router.post('/:id/cesta', secure.isAuthenticated, pedidosController.addPedido);
 
 module.exports = router;
